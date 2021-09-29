309ef4fe7d22328664903d67370b592a60be86ff
ishita302000 <ishitagoel3020@gmail.com>
2021-09-29 15:30:38 +0530

    Funtionality are converted to classes

---
+++ BankApplication/Account.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    class Account
+    {
+        private int acno;
+        public int useraccno
+        {
+            get { return acno; }
+            set { acno = value; }
+        }
+        protected string name;
+        internal int deposit;
+        protected char type;
+
+        protected void create_account()
+        {
+            Console.WriteLine("Enter your Account No. : ");
+            useraccno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Name of the Account Holder name : ");
+            name = Console.ReadLine();
+            Console.WriteLine(" Enter the Type of Account(C/S) : ");
+            type = char.Parse(Console.ReadLine());
+            Console.WriteLine(" Enter the Initial Amount : ");
+            deposit = int.Parse(Console.ReadLine());
+            Console.WriteLine("Account Created.....");
+
+        }
+        public void createaccount()
+        {
+            create_account();
+        }
+        protected void show_Account()
+        {
+            Console.WriteLine("\nAccount No. : {0} ", useraccno);
+            Console.WriteLine("\nAccount Holder Name : {0} ", name);
+            Console.WriteLine("\nType of Account : {0} ", type);
+            Console.WriteLine("\nBalance Amount : {0} ", deposit);
+        }
+        public void showAccount()
+        {
+            show_Account();
+        }
+        internal void modifyaccount()
+        {
+            Console.WriteLine("\nAccount No. : {0} ", acno);
+            Console.WriteLine("\nModify Account Holder Name : {0} ");
+            name = Console.ReadLine();
+            Console.WriteLine("\nModify Type of Account");
+            type = char.Parse(Console.ReadLine());
+            Console.WriteLine("\nModify Account Balance ");
+            deposit = int.Parse(Console.ReadLine());
+        }
+        public void modify_account()
+        {
+            modifyaccount();
+        }
+        public void accountdeposit()
+        {
+            int x;
+            Console.WriteLine("Enter the amount to be Deposited ::");
+            x = int.Parse(Console.ReadLine());
+            deposit += x;
+            Console.WriteLine(" Current Bank Balance is : {0}", deposit);
+        }
+        public void accountWithdraw()
+        {
+            int x;
+            Console.WriteLine("Enter the amount to be withdrawen ::");
+            x = int.Parse(Console.ReadLine());
+            if (x <= deposit && x >0 )
+            {
+                deposit -= x;
+                Console.WriteLine(" Current Bank Balance is : {0}", deposit);
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+        public void account_report()
+        {
+            Console.Write("Acno ::{0} \n Name ::{1}  \nType ::{2}  \n Deposit ::{3}\n" , acno , name , type , deposit);
+        }
+        public int retacno()
+        {
+            return acno;
+        }
+        public int retdeposit()
+        {
+            return deposit;
+        }
+        public char rettype()
+        {
+            return type;
+        }
+    }
+}
--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -6,99 +6,70 @@ using System.Threading.Tasks;
 
 namespace BankApplication
 {
-	public class Bank
-	{
-		public static void Main()
-		{
-			string username = " ";
-			Console.WriteLine("Enter username: ");
-			username = Console.ReadLine();
-			BankAccount newAccount = new BankAccount(username, 200);
-		}
-	}
-
-	public class BankAccount : Bank
-	{
-		List<double> DepositList = new List<double>();
-		//object[] DepositArr = new object[6];
-		public string username { get; set; }
-
-		public double Balance { get; set; }
-
-		public BankAccount(string username, double Balance)
-		{
-			this.username = username;
-			this.Balance = Balance;
-
-			Console.WriteLine("Username: {0} , Balance: {1}", username, Balance);
-			NextMenu();
-		}
-
-		public void NextMenu()
-		{
-			double balance = Balance;
-			Console.WriteLine("Enter 1 for Withdrawl, 2 for Deposit, 3 for Logout, 4 for Deposit History");
-			string StringMenu = Console.ReadLine();
-			int NextChoice = Convert.ToInt32(StringMenu);
-			switch (NextChoice)
-			{
-				case 1:
-					Withdraw();
-					break;
-				case 2:
-					Deposit();
-					break;
-				case 3:
-					Logout();
-					break;
-				case 4:
-					DepositHistory();
-					break;
-			}
-		}
-
-		public double Withdraw()
-		{
-			Console.WriteLine("Available Balance: {0}", Balance);
-			Console.WriteLine("How much would you like to withdraw?: ");
-			double WithdrawAmt = Convert.ToDouble(Console.ReadLine());
-			Balance = Balance -= WithdrawAmt;
-			Console.WriteLine( " Available Balance : " +Balance);
-			NextMenu();
-			return WithdrawAmt;
-		}
-
-		public double Deposit()
-		{
-			Console.WriteLine("Available Balance: {0}", Balance);
-			Console.WriteLine("How much would you like to deposit?: ");
-			double DepositAmt = Convert.ToDouble(Console.ReadLine());
-
-
-			DepositList.Add(DepositAmt);
-
-			Balance = Balance + DepositAmt;
-			Console.WriteLine( "Available balance : " + Balance);
-			NextMenu();
-			return DepositAmt;
-		}
-
-		public void Logout()
-		{
-			Console.WriteLine("Goodbye: " + username);
-		}
-
-		public void DepositHistory()
-		{
-			foreach (double i in DepositList)
-			{
-				Console.WriteLine("Deposit History: {0} " + i);
-				// 	Console.WriteLine();
-				Console.ReadKey();
-			}
-		}
-
-	}
+    class Program : Account
+    {
+        static void Main(string[] args)
+        {
+            int num, n; 
+            Account bank = new Account();
+            do
+            {
+                Console.Write("\n\n\nMain Menu");
+                Console.Write("\n\t 01. New Account");
+                Console.Write("\n\t 02. Deposit Amount");
+                Console.Write("\n\t 03. WithDraw Amount");
+                Console.Write("\n\t 04. Balance Enquiry");
+                Console.Write("\n\t 05. All Account Holder List ");
+                Console.Write("\n\t 06. Modify Account ");
+                Console.Write("\n\t 06. Exit ");
+                Console.Write("\n\t Select an option from 1-7 : ");
+                n = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (n)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            bank.createaccount();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Clear();
+                            bank.accountdeposit();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Clear();
+                            bank.accountWithdraw();
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.Clear();
+                            bank.account_report();
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.Clear();
+                            bank.showAccount();
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.Clear();
+                            Console.Write("\n\nEnter the Account no. : ");
+                            num = int.Parse(Console.ReadLine());
+                            bank.modify_account();
+                            break;
+                        }
+                }
+            }
+            while (n != 7);
+        }
+    }
 
 }
 
